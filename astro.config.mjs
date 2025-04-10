d607bf4411f2e5f1047416ccb907061bb28175ef
Eren Okka <erenokka@gmail.com>
2025-04-10 18:47:32 +0300

    Organize assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,37 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://melodi.dev',
   trailingSlash: 'never',
+  build: {
+    assets: 'assets',
+    inlineStylesheets: 'never',
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            const directories = {
+              fonts: ['ttf', 'woff', 'woff2'],
+              images: ['jpg', 'png', 'svg', 'webp'],
+            };
+            const ext = path
+              .extname(assetInfo.name)
+              .replace(/^\./, '')
+              .toLowerCase();
+            for (const key in directories) {
+              if (directories[key].includes(ext)) {
+                return `assets/${key}/[name]-[hash][extname]`;
+              }
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+    },
+  },
 });

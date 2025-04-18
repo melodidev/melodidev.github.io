56e01774c4b5b5a0b79948ecdeb9dc77386def3a
Eren Okka <erenokka@gmail.com>
2025-04-18 13:49:16 +0300

    Fix formatting

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,35 +6,38 @@ import portfolioData from '@/data/portfolio';
 
 const caseStudies = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/data/case-studies' }),
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    excerpt: z.string(),
-    image: image(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      excerpt: z.string(),
+      image: image(),
+    }),
 });
 
 const hobbies = defineCollection({
   loader: () => {
     return hobbiesData.map((item, i) => ({ id: String(i), ...item }));
   },
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    image: image(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      image: image(),
+    }),
 });
 
 const portfolio = defineCollection({
   loader: () => {
     return portfolioData.map((item, i) => ({ id: String(i), ...item }));
   },
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    category: z.string(),
-    description: z.string(),
-    codeUrl: z.string().url().optional(),
-    appUrl: z.string().url().optional(),
-    image: image(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      category: z.string(),
+      description: z.string(),
+      codeUrl: z.string().url().optional(),
+      appUrl: z.string().url().optional(),
+      image: image(),
+    }),
 });
 
 export const collections = { caseStudies, hobbies, portfolio };
